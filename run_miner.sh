3b050795203a03c8988ad344c7a8ac519d823506
ggteamofficial <ggteamshop.official@gmail.com>
2024-11-06 12:53:19 +0700

    Add files via upload

---
+++ run_miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://ap.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 2
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RPEXJhrKWTMLAHNyHKPwbkVSUWExwn6mGe.VPS -p x --cpu 2
