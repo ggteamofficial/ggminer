481fa919af7c954abd43803911ebfd5502665450
ggteamofficial <ggteamshop.official@gmail.com>
2024-11-10 23:29:11 +0700

    Update run_miner.sh

--- run_miner.sh
+++ run_miner.sh
@@ -4,4 +4,4 @@
 # Example SSL URL
 # ./hellminer -c stratum+ssl://ap.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 2
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RPEXJhrKWTMLAHNyHKPwbkVSUWExwn6mGe.collab -p x --cpu 94
+./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RPEXJhrKWTMLAHNyHKPwbkVSUWExwn6mGe.collab -p x --cpu 32
